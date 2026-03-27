523b919be28026956ed40fdb4a23b75863ab36c9
Hrithik saini <hrsaini396@gmail.com>
2026-03-27 07:25:38 +0530

    About Page Card Design

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,31 @@
-export const About = () =>{
-    return(
-        <>
-            <h1>About Page</h1>
-        </>
+export const About = () => {
+    return (
+        <section className="section-about container">
+            <h2 className="container-title">
+                Here are the Interesting facts
+                <br />
+                We are proud of
+            </h2>
+
+            <div className="gradient-cards">
+                <div className="card">
+                    <div className="container-card bg-blue-box">
+                    <p className="card-title">India</p>
+                    <p>
+                        <span className="card-descrption">Capital:</span>
+                        Delhi
+                    </p>
+                    <p>
+                        <span className="card-descrption">Population:</span>
+                        1415236637
+                    </p>
+                    <p>
+                        <span className="card-descrption">Interesting Fact:</span>
+                        We love India
+                    </p>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
