33d47ecbc522e51271922ab45cef0e61ce661358
Hrithik saini <hrsaini396@gmail.com>
2026-03-31 07:55:05 +0530

    Contact Form Page is Created and handle form submit thorugh object entries method

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,23 @@
-export const Contact = () =>{
-    return(
-        <>
-            <h1>Contact Page</h1>
-        </>
+export const Contact = () => {
+
+    // Contact form handling in react 19 version 
+
+    const handleformsubmit=(formData)=>{
+        // console.log(formData.entries())
+        const forminputdata = Object.fromEntries(formData.entries())
+        console.log(forminputdata)
+    }
+    return (
+        <section className="section-contact">
+            <h2 className="container-title">Contact</h2>
+            <div className="contact-wrapper container">
+                <form action={handleformsubmit} >
+                    <input type="text" required autoComplete="false" placeholder="Enter Your Username" name="username" className="form-control" />
+                    <input type="email" required autoComplete="false" placeholder="Enter Your Email" name="email" className="form-control" />
+                    <textarea className="form-control" rows="10" placeholder="Enter Your Message" name="message" required autoComplete="false" />
+                    <button type="submit" value="send">Send</button>
+                </form>
+            </div>
+        </section>
     )
 }
