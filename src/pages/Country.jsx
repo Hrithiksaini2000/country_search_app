68befd88ab68b384d6fae8ac6646a74610cfd8f3
Hrithik saini <hrsaini396@gmail.com>
2026-04-05 19:48:40 +0530

    New AXIOS post API file is created and data get in country page and new hook is used which use transition.

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,5 +1,23 @@
-export const Country = () =>{
-    return(
+import { useEffect } from "react"
+import { getCountryData } from "../api/postApi"
+import { useTransition } from 'react';
+
+
+export const Country = () => {
+
+    // New Hook Which is introduced in react js
+    const [isPending, startTransition] = useTransition()
+
+    useEffect(() => {
+        startTransition(async () => {
+            const res = await getCountryData()
+            console.log(res)
+        })
+    }, [])
+
+    if (isPending) return <h1>Loading.....</h1>
+
+    return (
         <>
             <h1>Country Page</h1>
         </>
