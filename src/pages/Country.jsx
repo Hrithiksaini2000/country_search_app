f3a41103304a523b62d253c6f0118870c2c4c392
Hrithik saini <hrsaini396@gmail.com>
2026-04-07 22:30:09 +0530

    New Loader file is created and new use state is created.

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,21 +1,24 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getCountryData } from "../api/postApi"
 import { useTransition } from 'react';
+import { Loader } from "../components/ui/Loader";
 
 
 export const Country = () => {
 
     // New Hook Which is introduced in react js
     const [isPending, startTransition] = useTransition()
+    const [country, setCountries] = useState([])
 
     useEffect(() => {
         startTransition(async () => {
             const res = await getCountryData()
             console.log(res)
+            setCountries(res.data)
         })
     }, [])
 
-    if (isPending) return <h1>Loading.....</h1>
+    if (isPending) return <Loader />
 
     return (
         <>
