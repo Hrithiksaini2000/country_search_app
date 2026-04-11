49ce18f7554f9bcb0e21973db2b24349fb6c3a3b
Hrithik saini <hrsaini396@gmail.com>
2026-04-11 12:21:33 +0530

    other data fetching from the api details like flags image, name , population, Region, Capital

--- src/components/layouts/Countrycard.jsx
+++ src/components/layouts/Countrycard.jsx
@@ -1,7 +1,29 @@
 export const Countrycard = ({ country }) => {
+
+    const { flags, name, capital, region, population } = country
+
     return (
-        <li>
-            {country.name.common}
+        <li className="country-card card">
+            <div className="container-card bg-white-box">
+                <img src={flags.svg} alt={flags.alt} />
+                <div className="country-info">
+                    <p className="card-title">
+                        {name.common.length > 10 ? name.common.slice(0, 10) + "..." : name.common}
+                    </p>
+                    <p>
+                        <span className="card-description">Population: </span>
+                        {population.toLocaleString()}
+                    </p>
+                    <p>
+                        <span className="card-description">Region: </span>
+                        {region}
+                    </p>
+                    <p>
+                        <span className="card-description">Capital: </span>
+                        {capital[0]}
+                    </p>
+                </div>
+            </div>
         </li>
     )
 }
